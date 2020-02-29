170c80ed9420249a59a782d17c2a39973627f22d
D1-92 <wyettlei@hotmail.com>
2020-02-29 16:38:03 +0800

    module juc-single-node test

--- juc-single-node/src/main/java/com/wyett/order/service/OrderService.java
+++ juc-single-node/src/main/java/com/wyett/order/service/OrderService.java
@@ -2,6 +2,7 @@ package com.wyett.order.service;
 
 import com.wyett.order.remoteService.RemoteServiceCall;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.Map;
 
@@ -11,6 +12,7 @@ import java.util.Map;
  * @description: TODO
  */
 
+@Service
 public class OrderService {
     @Autowired
     RemoteServiceCall remoteServiceCall;
